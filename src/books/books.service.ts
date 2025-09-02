879fa15e64eb6867f010a62e2ec98777486d909a
Nickolas <eagle.dev.stack@gmail.com>
2025-09-02 23:53:16 +0400

    Добавление функции пагинации страниц

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Book } from './schemas/book.schema';
 import { Author } from './schemas/author.schema';
 
@@ -15,7 +15,7 @@ export class BooksService {
     return this.bookModel.find().populate('author', 'name').exec();
   }
 
-  async createBook(title: string, authorId: string): Promise<Book> {
+  async createBook(title: string, authorId: Types.ObjectId): Promise<Book> {
     const book = new this.bookModel({ title, author: authorId });
     return book.save();
   }
@@ -27,5 +27,36 @@ export class BooksService {
   async createAuthor(name: string, country: string): Promise<Author> {
     const author = new this.authorModel({ name, country });
     return author.save();
+  }
+  async paginateBooks(page = 1, limit = 5) {
+    const skip = (page - 1) * limit;
+
+    const data = await this.bookModel.aggregate([
+      {
+        $lookup: {
+          from: 'authors',
+          localField: 'author',
+          foreignField: '_id',
+          as: 'author',
+        },
+      },
+      { $unwind: '$author' },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          author: '$author.name',
+        },
+      },
+      { $skip: skip },
+      { $limit: limit },
+    ]);
+
+    const totalCountAgg = await this.bookModel.aggregate([{ $count: 'total' }]);
+
+    const totalCount = totalCountAgg[0]?.total || 0;
+    const pagesCount = Math.ceil(totalCount / limit);
+
+    return { books: data, totalCount, pagesCount };
   }
 }
