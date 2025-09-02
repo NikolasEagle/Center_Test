d9ccd7c8b4f9dfc8736a9e725b92ed0ce66d3cdb
Nickolas <eagle.dev.stack@gmail.com>
2025-09-02 23:55:58 +0400

    Изменение маршрута получения информации о книгах

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 
 import faker from '@gwinkamp/faker';
 import { getRandomCountry } from 'countries-ts';
 import randomTitle from 'random-title';
+import { Book } from './schemas/book.schema';
 import { Types } from 'mongoose';
 
 @Controller()
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
+
   @Get()
-  async getBooks() {
-    return this.booksService.getBooks();
+  async getBooks(@Query('page') page: string) {
+    if (page && typeof Number(page) === 'number' && Number(page) > 0) {
+      const pageNumber = parseInt(page, 10) || 1;
+      const limitNumber = 5;
+
+      return this.booksService.paginateBooks(pageNumber, limitNumber);
+    } else {
+      return {
+        error: 'Missing parameter - page',
+      };
+    }
   }
   @Post('generateAuthors')
   async generateAuthors() {
@@ -29,8 +40,11 @@ export class BooksController {
     const authors = await this.booksService.getAuthors();
     for (let author of authors) {
       const title = randomTitle({ words: 4 });
-      const authorId = author._id instanceof Types.ObjectId && author._id;
-      this.booksService.createBook(title, authorId.toString());
+      const authorId: Book['author'] =
+        author._id instanceof Types.ObjectId && typeof author._id !== 'boolean'
+          ? author._id
+          : new Types.ObjectId();
+      this.booksService.createBook(title, authorId);
     }
     return 'Success!';
   }
