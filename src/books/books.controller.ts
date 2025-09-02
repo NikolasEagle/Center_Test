b8e5b5d2f74da86293d2bcd26b4047505939ff36
root <root@hmsrvr>
2025-09-02 22:20:44 +0400

    Добавление маршрута для генерации и записи произвольных данных в базу

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { BooksService } from './books.service';
+
+import faker from '@gwinkamp/faker';
+
+import { getRandomCountry } from 'countries-ts';
 
 @Controller()
 export class BooksController {
@@ -7,5 +11,16 @@ export class BooksController {
   @Get()
   async getBooks() {
     return this.booksService.getBooks();
+  }
+  @Post('generateAuthors')
+  async generateAuthors() {
+    for (let i = 1; i <= 250; i++) {
+      const name = faker.person.fullNameString();
+
+      const country = getRandomCountry().label;
+
+      this.booksService.createAuthor(name, country);
+    }
+    return 'Success!';
   }
 }
